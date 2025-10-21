3d589e1c2b7537eb9fae1ca1287e20e5dd656864
Leonardo Alves <datascileo@gmail.com>
2025-10-21 19:24:34 -0300

    feat(signup): signup's page (closes #02)

---
+++ src/main/java/com/github/datasleo/mothsinmywallet/exception/PasswordAndRepeatPasswordAreNotEqualsException.java
@@ -0,0 +1,9 @@
+package com.github.datasleo.mothsinmywallet.exception;
+
+public class PasswordAndRepeatPasswordAreNotEqualsException extends RuntimeException {
+    
+    public PasswordAndRepeatPasswordAreNotEqualsException(String message) {
+        super(message);
+    }
+
+}
